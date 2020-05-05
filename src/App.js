dd48e686e16893a7b4263a177e67999d0b22da9e
Sandeep Sharma <sandy335582@gmail.com>
2020-05-05 19:22:17 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CompleteUsers from "./components/CompleteUsers";
 import IncompleteUsers from "./components/IncompleteUSers";
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="container-fluid">
+        <div className="container">
+        <Navbar/>
           <div className="row">
             <div className="col-md-8">
-              <Route path="/completeids">
-                <CompleteUsers />
-              </Route>
-              <Route path="/incompleteids">
-                <IncompleteUsers />
-              </Route>
+              <Switch>
+                <Route path="/completeids">
+                  <CompleteUsers />
+                </Route>
+                <Route path="/incompleteids">
+                  <IncompleteUsers />
+                </Route>
+              </Switch>
             </div>
 
             <div className="col-md-4">update User column</div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+      <span className="navbar-brand">Random user</span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className='nav-item'>
+            <NavLink className='nav-link'  to="/completeids">Complete ids</NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link'  to="/incompleteids">InComplete ids</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
